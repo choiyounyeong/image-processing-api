c5501d65dc972bc41822c644362c82d64b33c2e8
choiyounyeong <young0770@naver.com>
2021-10-14 23:43:25 +0900

    add middleware and endpoints

--- src/index.ts
+++ src/index.ts
@@ -1,24 +1,51 @@
 import express from 'express';
 import path from 'path';
+import logger from './utilities/logger';
+import sharp from 'sharp';
+import { readdir } from 'fs/promises';
+import fs from 'fs';
 
 const app = express();
 const port = 3000;
-const sharp = require('sharp');
 
-app.get('/api/images', async (req, res) => {
+// Image API homepage
+app.get('/', (req, res) => {
+  res.send('This is the homepage for image handling');
+});
+
+// Provide the current image list
+app.get('/images/list', logger, async (req, res) => {
+  try {
+    const imageFolder = path.resolve(`images/full`);
+    const filenames = await readdir(imageFolder);
+    const imagenames = filenames.map(f => f.split('.')[0]);
+    res.send(imagenames);
+  } catch (err) {
+    res.send('There is an error');
+  }
+});
+
+// Provide resized image
+app.get('/images/resize', logger, async (req, res) => {
   const filename = req.query.filename;
   const height = req.query.height;
   const width = req.query.width;
   const originalFilepathRel = `images/full/${filename}.jpg`;
   const originalFilepathAbs = path.resolve(originalFilepathRel);
-  const targetFilepathRel = `images/thumb/${filename}.jpg`;
-  const targetFilepathAbs = path.resolve(targetFilepathRel);
-  await sharp(originalFilepathAbs)
-    .resize(Number(width), Number(height))
-    .toFile(targetFilepathAbs);
-  res.sendFile(targetFilepathAbs);
+  if (fs.existsSync(originalFilepathAbs)) {
+    const targetFilepathRel = `images/thumb/${filename}.jpg`;
+    const targetFilepathAbs = path.resolve(targetFilepathRel);
+    await sharp(originalFilepathAbs)
+      .resize(Number(width), Number(height))
+      .toFile(targetFilepathAbs);
+    res.sendFile(targetFilepathAbs);
+  } else {
+    res.status(400);
+    res.send('Image does not exist');
+  }
 });
 
+// Print message in terminal
 app.listen(port, () => {
   console.log(`server started at localhost: ${port}`);
 });
