6dcff2674d1a4fc70cab02fcda63de2faf056ac8
choiyounyeong <young0770@naver.com>
2021-10-16 13:13:01 +0900

    Use router

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,6 @@
 import express from 'express';
 import { Request, Response } from 'express';
-import path from 'path';
-import logger from './utilities/logger';
-import sharp from 'sharp';
-import { readdir } from 'fs/promises';
-import fs from 'fs';
+import imageRoute from './routes/images';
 
 const app = express();
 const port = 3000;
@@ -17,63 +13,7 @@ app.get(
   }
 );
 
-// Provide the current image list
-app.get(
-  '/images/list',
-  logger,
-  async (req: Request, res: Response): Promise<void> => {
-    try {
-      const imageFolder = path.resolve(`images/full`);
-      const filenames = await readdir(imageFolder);
-      const imagenames = filenames.map(f => f.split('.')[0]);
-      res.send(imagenames);
-    } catch (err) {
-      res.send('There is an error');
-    }
-  }
-);
-
-// Provide resized image
-app.get(
-  '/images/resize',
-  logger,
-  async (req: Request, res: Response): Promise<void> => {
-    const filename = req.query.filename;
-    const height = Number(req.query.height);
-    const width = Number(req.query.width);
-
-    if (isNaN(height) || isNaN(width)) {
-      res.status(400);
-      res.send('Invalid height and width');
-      return;
-    }
-
-    const originalFilepathRel = `images/full/${filename}.jpg`;
-    const originalFilepathAbs = path.resolve(originalFilepathRel);
-    if (fs.existsSync(originalFilepathAbs)) {
-      const imageSaveFolder = 'images/thumb';
-      const targetFilepathRel = `${imageSaveFolder}/${filename}-${width}-${height}.jpg`;
-      const targetFilepathAbs = path.resolve(targetFilepathRel);
-
-      if (!fs.existsSync(imageSaveFolder)) {
-        fs.mkdirSync(imageSaveFolder);
-      }
-
-      // if already exists(cached), just return
-      if (fs.existsSync(targetFilepathAbs)) {
-        res.sendFile(targetFilepathAbs);
-      } else {
-        await sharp(originalFilepathAbs)
-          .resize(width, height)
-          .toFile(targetFilepathAbs);
-        res.sendFile(targetFilepathAbs);
-      }
-    } else {
-      res.status(400);
-      res.send('Image does not exist');
-    }
-  }
-);
+app.use('/images', imageRoute);
 
 app.listen(port);
 
---
+++ src/routes/images.ts
@@ -0,0 +1,67 @@
+import express from 'express';
+import { Request, Response } from 'express';
+import path from 'path';
+import logger from '../utilities/logger';
+import sharp from 'sharp';
+import { readdir } from 'fs/promises';
+import fs from 'fs';
+const imageRoute = express.Router();
+
+// Provide the current image list
+imageRoute.get(
+  '/list',
+  logger,
+  async (req: Request, res: Response): Promise<void> => {
+    try {
+      const imageFolder = path.resolve(`images/full`);
+      const filenames = await readdir(imageFolder);
+      const imagenames = filenames.map(f => f.split('.')[0]);
+      res.send(imagenames);
+    } catch (err) {
+      res.send('There is an error');
+    }
+  }
+);
+
+// Provide resized image
+imageRoute.get(
+  '/resize',
+  logger,
+  async (req: Request, res: Response): Promise<void> => {
+    const filename = req.query.filename;
+    const height = Number(req.query.height);
+    const width = Number(req.query.width);
+
+    if (isNaN(height) || isNaN(width)) {
+      res.status(400);
+      res.send('Invalid height and width');
+      return;
+    }
+
+    const originalFilepathRel = `images/full/${filename}.jpg`;
+    const originalFilepathAbs = path.resolve(originalFilepathRel);
+    if (fs.existsSync(originalFilepathAbs)) {
+      const imageSaveFolder = 'images/thumb';
+      const targetFilepathRel = `${imageSaveFolder}/${filename}-${width}-${height}.jpg`;
+      const targetFilepathAbs = path.resolve(targetFilepathRel);
+
+      if (!fs.existsSync(imageSaveFolder)) {
+        fs.mkdirSync(imageSaveFolder);
+      }
+
+      // if already exists(cached), just return
+      if (fs.existsSync(targetFilepathAbs)) {
+        res.sendFile(targetFilepathAbs);
+      } else {
+        await sharp(originalFilepathAbs)
+          .resize(width, height)
+          .toFile(targetFilepathAbs);
+        res.sendFile(targetFilepathAbs);
+      }
+    } else {
+      res.status(400);
+      res.send('Image does not exist');
+    }
+  }
+);
+export default imageRoute;
